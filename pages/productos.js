a863bfbf75938d903291826412ada0d05e17586b
Diego Saleh <diego.alesaleh1@gmail.com>
2024-09-01 14:30:09 -0300

    Modificacion en la tabla de clientes, para que sea responsive

--- pages/productos.js
+++ pages/productos.js
@@ -39,26 +39,28 @@ const Productos = () => {
           </a>
         </Link>
 
-        <table className="table-auto shadow-md mt-10 w-full w-lg">
-          <thead className="bg-gray-800">
-            <tr className="text-white">
-              <th className="w-1/5 py-2">Nombre</th>
-              <th className="w-1/5 py-2">Existencia</th>
-              <th className="w-1/5 py-2">Precio</th>
-              <th className="w-1/5 py-2">Eliminar</th>
-              <th className="w-1/5 py-2">Editar</th>
-            </tr>
-          </thead>
+        <div className="overflow-x-scroll">
+          <table className="table-auto shadow-md mt-10 w-full w-lg">
+            <thead className="bg-gray-800">
+              <tr className="text-white">
+                <th className="w-1/5 py-2">Nombre</th>
+                <th className="w-1/5 py-2">Existencia</th>
+                <th className="w-1/5 py-2">Precio</th>
+                <th className="w-1/5 py-2">Eliminar</th>
+                <th className="w-1/5 py-2">Editar</th>
+              </tr>
+            </thead>
 
-          <tbody className="bg-white">
-            {data.obtenerProductos.map( producto =>(
-              <Producto 
-                key={producto.id}
-                producto={producto}
-              />
-            ))}
-          </tbody>
-        </table>
+            <tbody className="bg-white">
+              {data.obtenerProductos.map( producto =>(
+                <Producto 
+                  key={producto.id}
+                  producto={producto}
+                />
+              ))}
+            </tbody>
+          </table>
+        </div>
       </Layout>
     </div>
   );
